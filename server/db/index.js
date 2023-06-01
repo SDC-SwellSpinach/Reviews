df8c58bccd545e14201b3f835e44260786db1343
Blake Hansen <b.hansen9264@gmail.com>
2023-06-01 14:30:13 -0700

    Added for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,8 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const { Client } = require('pg');
 
+console.log(process.env.HOST, process.env.PORT);
 const client = new Client({
   database: 'reviews',
 });
--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
