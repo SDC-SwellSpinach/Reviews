ea12d094c32875f11b433bbdf3ef451c84f7f20c
Blake Hansen <b.hansen9264@gmail.com>
2023-06-03 09:47:43 -0700

    Adding LoaderIO token back into routes

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // app.use(morgan('tiny'));
 
 app.use(cors());
-// app.get('/loaderio-a8089335daa66efc9b2b4dcecc69b4ac', (req, res) => res.send('loaderio-a8089335daa66efc9b2b4dcecc69b4ac'));
+app.get('/loaderio-a8089335daa66efc9b2b4dcecc69b4ac', (req, res) => res.send('loaderio-a8089335daa66efc9b2b4dcecc69b4ac'));
 app.use('/', router);
 
 module.exports = app;
