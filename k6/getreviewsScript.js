0170fe36c11f422427da607520e773fd5eb3f510
Blake Hansen <b.hansen9264@gmail.com>
2023-06-03 08:57:21 -0700

    Added Indexing with redis

--- k6/getreviewsScript.js
+++ k6/getreviewsScript.js
@@ -6,15 +6,15 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { vus: 5 },
-    { duration: '10s', target: 500 },
+    { vus: 1 },
+    { duration: '10s', target: 50 },
     { duration: '30s', target: 1000 },
-    { duration: '10s', target: 500 },
+    { duration: '10s', target: 50 },
   ],
 };
 
 export default function () {
-  const productID = Math.floor(Math.random() * 100000 + 10000);
+  const productID = Math.floor(Math.random() * 10000 + 10000);
   const res = http.get(`http://localhost:3000/reviews?page=1&count=5&sort=helpful&product_id=${productID}`, {
     tags: { name: 'Get Reviews for given product' },
   });
--- k6/postreviewsScript.js
+++ k6/postreviewsScript.js
@@ -7,14 +7,14 @@ import { check, sleep } from 'k6';
 export const options = {
   stages: [
     { vus: 5 },
-    { duration: '10s', target: 500 },
-    { duration: '30s', target: 10000 },
-    { duration: '10s', target: 500 },
+    { duration: '10s', target: 5 },
+    { duration: '30s', target: 100 },
+    { duration: '10s', target: 50 },
   ],
 };
 
 export default function () {
-  const productID = Math.floor(Math.random() * 100000 + 10000);
+  const productID = Math.floor(Math.random() * 10000 + 1000);
   const data = {
     product_id: productID,
     rating: 4,
